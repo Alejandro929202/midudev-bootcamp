f6fae878ce737c3c16d3bf133fc35e159998fa67
alejandro pm <alexpm_1992@hotmail.com>
2021-09-13 22:02:50 +0200

    subida13092021

--- part3-react/src/index.js
+++ part3-react/src/index.js
@@ -1,17 +1,37 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import { useState } from 'react';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+const rootElement = document.getElementById('root');
+
+const App = (props) =>{
+  const [contadorValue, updateContador] = useState(13);
+  
+  /*
+  const contador = useState(5);
+  const contadorValue = contador[0];
+  const updateContador = contador[1];
+  */
+
+  setInterval(() => {
+    updateContador(contadorValue + 1)
+  }, 2000);
+
+  console.log("render");
+
+  return (
+    <div>
+      <p>el valor del contador es: </p>
+      <h1>{contadorValue}</h1>
+      <h2>Magia de React</h2>
+    </div>
+  );
+};
+
+
+ReactDOM.render(<App />, rootElement);
+
+
+

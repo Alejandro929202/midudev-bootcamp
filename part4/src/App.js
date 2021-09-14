caccaf368960093e8f5241ce01f2723752df64aa
alejandro pm <alexpm_1992@hotmail.com>
2021-09-14 22:48:46 +0200

    formulario1

--- part4/src/App.js
+++ part4/src/App.js
@@ -1,25 +1,52 @@
-import logo from './logo.svg';
 import './App.css';
+import { Note } from './Note.js';
+import { useState } from 'react';
 
-function App() {
+
+/*
+{notes.map((note) => {
+      return <p><strong>{note.id}</strong></p>;
+    })}
+*/
+
+
+
+export default function App(props){
+  const [notes, setNotes] = useState(props.notes);
+  const [newNote, setNewNote] = useState("");
+
+  const handleChange = (event) => {
+    setNewNote(event.target.value);
+  }
+
+  const handleClick = (event) => {
+    console.log('crear nota');
+    const noteToAddToState = {
+      id:notes.lenght + 1,
+      content: newNote,
+      date: new Date().toISOString(),
+      important: Math.random() < 0.5
+    };
+
+    setNotes(notes.concat(noteToAddToState));
+    //setNotes([...notes, noteToAddToState]) MEJOR FORMA CON SPREND
+    setNewNote("");
+
+  }
+
+  if(typeof notes === "undefined" || notes.lenght === 0){
+    return "No tenemos notas que mostrar";
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+  <div>
+    <h1>Notes</h1>
+      <ol>
+          {notes.map((note) => <Note key={note.id} {...note} />)}
+      </ol>
+      <div>
+          <input type='text' onChange={handleChange} />
+          <button onClick={handleClick}>Crear nota</button>
+      </div>
+  </div>
   );
-}
-
-export default App;
+};
--- part4/src/index.js
+++ part4/src/index.js
@@ -4,12 +4,33 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-); 
+const notes = [
+  {
+    //objeto1
+    id:1,
+    content: 'HTML is easy',
+    date: '2019-0530T17:30:31.098Z',
+    important: true,
+    categories:['sport', 'hobby']
+  },
+  {
+    //objeto2
+    id:2,
+    content: 'Browser can execute only Javascript',
+    date: '2019-0530T17:30:31.091Z',
+    important: false
+  },
+  {
+    //objeto3
+    id:3,
+    content: 'GET and POST are the most important methods of ....',
+    date: '2019-0530T17:30:31.298Z',
+    important: true
+  }
+];
+
+const rootElement = document.getElementById('root');
+ReactDOM.render(<App notes={notes} />, rootElement); 
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))

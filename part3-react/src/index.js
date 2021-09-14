48b51805c28de4be040b347d8f35b1fbec6040ed
alejandro pm <alexpm_1992@hotmail.com>
2021-09-14 18:36:17 +0200

    commit Renderizado condicional

--- part3-react/src/index.js
+++ part3-react/src/index.js
@@ -3,20 +3,56 @@ import ReactDOM from 'react-dom';
 import "./style.css";
 import { useState } from 'react';
 
+//Renderizado condicional -> componente de aviso 
+const WarningNotUsed = () => {
+    return <h1>Todavía no se ha usado el contador</h1>
+}
+
+//Renderizado condicional -> componente de listado de clicks
+const ListOfClicks = ({clicks}) => {
+    return <p>{clicks.join(", ")}</p>
+}
+
 const App = () => { 
-    const [left, setLeft] = useState(0)
-    const [right, setRight] = useState(0)
+    //const [left, setLeft] = useState(10)
+    //const [right, setRight] = useState(23)
+
+    //Estado complejo con objetos
+    const [counters, setCounters] = useState({
+        left: 0,
+        right: 0,
+        mensaje: 'Mensaje en el estado'
+    })
+
+    //Estado con array
+    const [clicks, setClicks] = useState([])
+
+    const handleClickLeft = () =>{
+        const newCounterState = {
+            ...counters, //carga todos las variables del estado y sobreescribe las nuevas
+            left: counters.left + 1,
+        }
+        setCounters(newCounterState); // Podemos crear una const con los nuevos valores y pasarselo al estado
+        setClicks((prevClicks) => [...prevClicks, "L"]);
+    }
+
+    const handleClickRight = () =>{
+        setCounters({
+            ...counters,
+            right: counters.right + 1,
+        })
+        setClicks((prevClicks) => [...prevClicks, 'R']);
+    }
 
     return(
         <div>
-            {left}
-            <button onClick={() => setLeft(left + 1)}>
-                left
-            </button>
-            <button onClick={() => setRight(right + 1)}>
-            right
-            </button>
-            {right}
+            {counters.left}
+            <button onClick={handleClickLeft}>left</button>
+            <button onClick={handleClickRight}>right</button>
+            {counters.right}
+            <p>Clicks totales: {clicks.length}</p>
+            <p>{counters.mensaje}</p>
+            {clicks.length === 0 ? (<WarningNotUsed />) : (<ListOfClicks clicks={clicks} />)}
         </div>
     )
 }

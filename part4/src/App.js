336c3bc6958fa64d1bc3b2a6a10fa9e73b1bb730
alejandro pm <alexpm_1992@hotmail.com>
2021-09-16 21:55:04 +0200

    commit axios

--- part4/src/App.js
+++ part4/src/App.js
@@ -1,18 +1,43 @@
 import './App.css';
 import { Note } from './Note.js';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
+import axios from 'axios';
+import { getAllNotes } from "./services/notes/getAllNotes";
+import { createNote } from './services/createNode';
 
 
 /*
 {notes.map((note) => {
       return <p><strong>{note.id}</strong></p>;
     })}
+
+    fetch('https://jsonplaceholder.typicode.com/posts')
+    .then((response) => response.json())
+    .then((json) =>{
+      console.log(json);
+      setNotes(json)
+      setLoading(false);
+    });
+  }, [])
 */
 
-export default function App(props){
-  const [notes, setNotes] = useState(props.notes);
+export default function App(){
+  const [notes, setNotes] = useState([]);
   const [newNote, setNewNote] = useState("");
   //const [showAll, setShowAll] = useState(true);
+  const [loading, setLoading] = useState(false);
+
+  useEffect(() =>{
+    console.log("useEffect");
+    setLoading(true);
+    getAllNotes()
+    .then((notes) => {
+      setNotes(notes);
+      setLoading(false);
+    });
+  }, [])
+
+  
 
   const handleChange = (event) => {
     setNewNote(event.target.value);
@@ -22,12 +47,17 @@ export default function App(props){
     event.preventDefault();
     console.log('crear nota');
     const noteToAddToState = {
-      id:notes.lenght + 1,
       title: newNote,
-      body: newNote
+      body: newNote,
+      userId: 1
     };
 
-    setNotes(notes.concat(noteToAddToState));
+    createNote(noteToAddToState)
+    .then(newNote => {
+      setNotes((prevNotes) => prevNotes.concat(data));
+    })
+
+    //setNotes(notes.concat(noteToAddToState));
     //setNotes([...notes, noteToAddToState]) MEJOR FORMA CON SPREND
     setNewNote("");
 
@@ -49,6 +79,7 @@ export default function App(props){
   return (
   <div>
     <h1>Notes</h1>
+    { loading ? "Cargando..." : ""}
     
       <ol>
           {notes

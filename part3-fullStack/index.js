531e6fe87d1be0df0eb5712735ca23180b4962db
alejandro pm <alexpm_1992@hotmail.com>
2021-09-21 19:42:14 +0200

    commit notes

--- part3-fullStack/index.js
+++ part3-fullStack/index.js
@@ -1,5 +1,25 @@
 const http = require('http')
 
+let notes = [
+    {
+        "id": 1,
+        "content": "sunt aut facere repellat provident occaecati excepturi optio reprehenderit",
+        "date": "2019-05-30t17:30:31.098Z",
+        "important": "true"
+      },
+      {
+        "id": 2,
+        "content": "qui est esse",
+        "date": "2019-05-30t17:30:31.098Z",
+        "important": "false"
+      },
+      {
+        "id": 3,
+        "content": "ea molestias quasi exercitationem repellat qui ipsa sit aut",
+        "date": "2019-05-30t17:30:31.098Z",
+        "important": "true"
+      }
+]
 
 //creamos un servidor y le pasamos un parametro que es un callBack(funcion que se ejecuta cada vez que le llega una request) 
 const app = http.createServer((request, response) => {

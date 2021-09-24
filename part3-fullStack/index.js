2f7a30ab62cb4eadc4b3d2896298588ba8b2f137
alejandro pm <alexpm_1992@hotmail.com>
2021-09-24 23:30:12 +0200

    commit nodejs end

--- part3-fullStack/index.js
+++ part3-fullStack/index.js
@@ -3,6 +3,10 @@ const express = require('express')
 const app = express()
 //const http = require('http')
 
+//Con esta linea decimos que App soporte las request cuando se le pase un objeto y lo
+// parsea el request.body para 
+app.use(express.json())
+
 let notes = [
     {
         "id": 1,
@@ -51,11 +55,30 @@ app.get('/api/notes/:id', (request, response) => {
     }
 })
 
-app.delete('/api/notes/:id', (request, response) => {
+const ids = notes.map(note => note.id)
+const maxId = Math.max(...ids)
+
+app.post('/api/notes', (request, response) => {
+    const note = request.body
+    
+    const newNote = {
+        id: maxId + 1,
+        content: note.content,
+        important: typeof note.important !== 'undefined' ? note.important : false,
+        date: new Date().toISOString()
+    }
+
+    notes = notes.concat(newNote)
+
+    response.json(newNote)
+})
+
+app.post('/api/notes/:id', (request, response) => {
     const id = Number(request.params.id)
-    note = notes.filter(note => note.id != id)
+    notes = notes.filter(note => note.id != id)
     response.status(204).end()
 })
+
 
 const PORT = 3001
 app.listen(PORT, () =>{

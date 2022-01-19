90289b6fb1003e8de9a616ac9580c4118511e657
alejandro pm <alexpm_1992@hotmail.com>
2022-01-19 21:10:37 +0100

    subida

--- part3-fullStack/index.js
+++ part3-fullStack/index.js
@@ -1,32 +1,33 @@
+//importar la conexion del fichero mongo.js
+require('./mongo') 
+const Note = require('./models/Note')
+
 //const { request } = require('express')
+const { request, response } = require('express')
 const express = require('express')
+const cors = require('cors')
 const app = express()
+const logger = require('./logger')
+
+
 //const http = require('http')
 
 //Con esta linea decimos que App soporte las request cuando se le pase un objeto y lo
 // parsea el request.body para 
 app.use(express.json())
 
-let notes = [
-    {
-        "id": 1,
-        "content": "sunt aut facere repellat provident occaecati excepturi optio reprehenderit and Twitch",
-        "date": "2019-05-30t17:30:31.098Z",
-        "important": "true"
-      },
-      {
-        "id": 2,
-        "content": "qui est esse",
-        "date": "2019-05-30t17:30:31.098Z",
-        "important": "false"
-      },
-      {
-        "id": 3,
-        "content": "ea molestias quasi exercitationem repellat qui ipsa sit aut",
-        "date": "2019-05-30t17:30:31.098Z",
-        "important": "true"
-      }
-]
+//CORS middleware que permite decir a nuestra API de que origenes permitimos acceder
+app.use(cors())
+
+app.use(logger)
+
+
+let notes = [{
+    id: 1,
+    content: 'Repasar los retos de JS de middudev',
+    date: '2019-05-30T19:20:14.298Z',
+    important: true
+}]
 
 //creamos un servidor y le pasamos un parametro que es un callBack(funcion que se ejecuta cada vez que le llega una request) 
 /*const app = http.createServer((request, response) => {
@@ -48,19 +49,34 @@ app.get('/api/notes/:id', (request, response) => {
     const note = notes.find(note => note.id === id)
 
     //si no encuentra notas devuelve un error 404
-    if(notes) {
+    if(note) {
         response.json(note)
     } else {
         response.status(404).end()
     }
 })
 
-const ids = notes.map(note => note.id)
-const maxId = Math.max(...ids)
+
+
+app.delete('/api/notes/:id', (request, response) => {
+    const id = Number(request.params.id)
+    const notes = notes.filter(note => note.id !== id)
+    response.status(204).end()
+})
 
 app.post('/api/notes', (request, response) => {
     const note = request.body
     
+    if(!note || !note.content){
+        return response.status(400).json({
+            error: 'note.content is missing'
+        })
+    }
+
+    const ids = notes.map(note => note.id)
+    const maxId = Math.max(...ids)
+
+
     const newNote = {
         id: maxId + 1,
         content: note.content,
@@ -70,7 +86,7 @@ app.post('/api/notes', (request, response) => {
 
     notes = notes.concat(newNote)
 
-    response.json(newNote)
+    response.status(201).json(newNote)
 })
 
 app.post('/api/notes/:id', (request, response) => {
@@ -79,6 +95,11 @@ app.post('/api/notes/:id', (request, response) => {
     response.status(204).end()
 })
 
+app.use((request, response) => {
+    response.status(404).json({
+        error: 'Not found'
+    })
+})
 
 const PORT = 3001
 app.listen(PORT, () =>{

a9d303a73f64d76b74fbffbb5b774c24bc6e5536
alejandro pm <alexpm_1992@hotmail.com>
2022-01-21 17:27:28 +0100

    Funciona JEST

---
+++ part3-fullStack/tests/notes.test.js
@@ -0,0 +1,12 @@
+const { application } = require('express')
+const supertest = require ('supertest')
+const app = require('../index')
+
+const api = supertest(app)
+
+test('notes are returned as json', () =>{
+    api
+        .get('/api/notes')
+        .expect(200)
+        .expect('Content-Type', /application\/json/)
+})
